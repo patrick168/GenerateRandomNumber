4a6b1113a5c544f39d33b9d39f67c904cac6f45c
Tung <koala5858@gmail.com>
2019-05-06 11:55:44 +0800

    完成基本功能

--- GenerateRandomNumber/WinFormRandom/Form1.Designer.cs
+++ GenerateRandomNumber/WinFormRandom/Form1.Designer.cs
@@ -28,50 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textRandom = new System.Windows.Forms.TextBox();
             this.btnran = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.btnGenAll = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textRandom
-            // 
-            this.textRandom.Enabled = false;
-            this.textRandom.Font = new System.Drawing.Font("PMingLiU", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textRandom.Location = new System.Drawing.Point(65, 25);
-            this.textRandom.Multiline = true;
-            this.textRandom.Name = "textRandom";
-            this.textRandom.Size = new System.Drawing.Size(475, 307);
-            this.textRandom.TabIndex = 0;
-            this.textRandom.Text = "NO";
             // 
             // btnran
             // 
-            this.btnran.Location = new System.Drawing.Point(117, 362);
+            this.btnran.Location = new System.Drawing.Point(117, 364);
             this.btnran.Name = "btnran";
-            this.btnran.Size = new System.Drawing.Size(75, 23);
+            this.btnran.Size = new System.Drawing.Size(114, 23);
             this.btnran.TabIndex = 1;
-            this.btnran.Text = "產生亂數";
+            this.btnran.Text = "產生一個亂數";
             this.btnran.UseVisualStyleBackColor = true;
             this.btnran.Click += new System.EventHandler(this.btnran_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.Font = new System.Drawing.Font("PMingLiU", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 27;
+            this.listBoxResult.Location = new System.Drawing.Point(117, 30);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(138, 166);
+            this.listBoxResult.TabIndex = 3;
+            // 
+            // btnGenAll
+            // 
+            this.btnGenAll.Location = new System.Drawing.Point(269, 364);
+            this.btnGenAll.Name = "btnGenAll";
+            this.btnGenAll.Size = new System.Drawing.Size(107, 23);
+            this.btnGenAll.TabIndex = 4;
+            this.btnGenAll.Text = "產生所有亂數";
+            this.btnGenAll.UseVisualStyleBackColor = true;
+            this.btnGenAll.Click += new System.EventHandler(this.btnGenAll_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGenAll);
+            this.Controls.Add(this.listBoxResult);
             this.Controls.Add(this.btnran);
-            this.Controls.Add(this.textRandom);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textRandom;
         private System.Windows.Forms.Button btnran;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Button btnGenAll;
     }
 }
 
--- GenerateRandomNumber/WinFormRandom/Form1.cs
+++ GenerateRandomNumber/WinFormRandom/Form1.cs
@@ -12,7 +12,7 @@ namespace WinFormRandom
 {
     public partial class Form1 : Form
     {
-        List<int> numbers = new List<int>();
+        List<int> rNumbers = new List<int>();       
         static int qt;
 
         public Form1()
@@ -22,28 +22,59 @@ namespace WinFormRandom
 
         private void btnran_Click(object sender, EventArgs e)
         {
-            int i;
-            Random random = new Random();
-
-            if (numbers.Count == 0)
-            {
-                for (i = 1; i <= qt; i++)
-                {
-                    numbers.Add(i);
-                }
-            }
-
-            int rnum = numbers[random.Next(qt)];
-           
-            textRandom.Text += numbers[rnum].ToString()+"\t";
-            qt--;
-            numbers.RemoveAt(rnum);
+            GenerateNumbersByOrder();
+            GenerateRandomNumbers();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            qt = 10;
-            textRandom.Text = string.Empty;
+            qt = 10;           
+        }
+
+        private void GenerateRandomNumbers()
+        {
+            Random random = new Random();
+            if (qt > 0)
+            {
+                int rOrder = random.Next(qt);
+
+                listBoxResult.Items.Insert(0, rNumbers[rOrder].ToString());
+                qt--;
+                rNumbers.Remove(rNumbers[rOrder]);
+            }
+            else
+            {
+                MessageBox.Show("亂數已經全部產生完");
+            }
+        }
+
+        private void GenerateNumbersByOrder()
+        {
+            int i;            
+
+            if (rNumbers.Count == 0)
+            {
+                for (i = 1; i <= qt; i++)
+                {
+                    rNumbers.Add(i);
+                }
+            }
+
+          
+        }
+
+        private void shuffle()
+        {
+
+        }
+
+        private void btnGenAll_Click(object sender, EventArgs e)
+        {
+            GenerateNumbersByOrder();
+            while (qt > 0)
+            {
+                GenerateRandomNumbers();
+            }
         }
     }
 }
